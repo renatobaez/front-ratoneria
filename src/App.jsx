49d5b2804240ef0ea27e04d0f6021cfdfe5cb4b5
Renato Francisco Báez Lobos <panchobaez@gmail.com>
2024-03-13 13:59:54 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="h-screen bg-slate-700 text-white p-10">
+      <h1 className="text-3xl blond underline">
+        Bienvenido a La Ratonería
+      </h1>
+      <br />
+      <h4 className="text-1xl">
+        En la Ratonería encontrarás los mejores precios para pasarla bien
+      </h4>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+}
