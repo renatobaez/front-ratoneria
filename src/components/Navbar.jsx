1e8f0c238f580b81f710e27f3cb12927b7d32af4
Javier Figueroa <javiernfigueroa@gmail.com>
2024-03-14 22:46:46 -0300

    routes/app.jsx-config

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <>
+      <p>Navbar</p>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/views/Profile.jsx
@@ -0,0 +1,9 @@
+function Profile() {
+  return (
+    <>
+      <p>Profile</p>
+    </>
+  );
+}
+
+export default Profile;
