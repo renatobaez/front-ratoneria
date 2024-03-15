834b518f1d8f5a3bfa6f7e66cc2b1e680a4e53e5
Javier Figueroa <javiernfigueroa@gmail.com>
2024-03-15 20:54:10 -0300

    varibles colores taiwind.config

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+
 function Home() {
   return (
     <>
       <p>Home</p>
+      <p>Variables de colores en el archivo tailwind.config.js</p>
+      <div className="flex">
+        <div className="w-10 h-20 bg-porange mr-2"></div>
+        <div className="w-10 h-20 bg-pgrey mr-2"></div>
+        <div className="w-10 h-20 bg-pdark-grey mr-2"></div>
+        <div className="w-10 h-20 bg-pdark"></div>
+      </div>
     </>
   );
 }
