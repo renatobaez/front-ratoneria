b854fdf6ee7400d0829e7fd339e8883f67b74450
samkee1001~ <samkeetangcl@gmail.com>
2024-03-20 20:26:34 -0300

    Feat Register

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+function card() {
+    return (
+        <>
+            <div className="border border-blue-500">
+                <p>Card</p>
+            </div>
+        </>
+    );
+}
+export default card
