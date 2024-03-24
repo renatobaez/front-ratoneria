887b8d51e37007291364a9ccfde80c50dd2f4067
Javier Figueroa <javiernfigueroa@gmail.com>
2024-03-24 18:36:37 -0300

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,9 @@ import AppRoutes from "./routes/AppRoutes";
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        {" "}
-        {/* ESTE DIV, se encarga de tomar TODA LA PAGINA NAV MAIN Y FOOTER Y USAR FLEX PARA ORDENARLA EN 3
-        SECCIONES LAS CUALES DEJE PINTADAS PARA QUE LAS VEAN */}
+      <div className="flex flex-col h-screen">
         <Navbar />
-        <main className="flex-1">
-          {" "}
-          {/* EL MAIN VA A QUEDAR POR AHORA OCUPANDO SIEMPRE TODO EL ESPACIO ENTRE EL NAVBAR Y EL FOOTER*/}
+        <main className=" flex-grow border-2">
           <AppRoutes />
         </main>
         <Footer />
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Carousel from "../components/Carousel";
 import Sidebar from "../components/Sidebar";
 import Searchbar from "../components/ui/Searchbar";
+import Card from "../components/Card";
+import Gallery from "../components/Gallery";
 
 function Home() {
   let slides = [
@@ -22,43 +24,42 @@ function Home() {
   };
 
   return (
-    <>
-      <div className="flex h-screen sm:w-full relative">
+    <div className="flex sm:w-full relative border-2">
+      <div
+        className={`w-1/6 bg-pgrey ${
+          isSidebarExpanded ? "block" : "w-12"
+        } md:block sm:absolute top-0 left-0 h-full z-10 transition-all duration-300`}
+      >
         <div
-          className={`w-1/6 bg-pgrey ${
-            isSidebarExpanded ? "block" : "w-12"
-          } md:block sm:absolute top-0 left-0 h-full z-10 transition-all duration-300`}
+          className="flex flex-col justify-center h-full cursor-pointer"
+          onClick={toggleSidebar}
         >
-          <div
-            className="flex flex-col justify-center h-full cursor-pointer"
-            onClick={toggleSidebar}
-          >
-            <span className="text-white text-sm font-semibold px-2 transform -rotate-90">
-              FILTROS
-            </span>
-          </div>
-        </div>
-        {isSidebarExpanded && (
-          <div className="w-1/6 bg-porange md:block sm:absolute top-0 left-0 h-full z-10 transition-all duration-300 relative">
-            <Sidebar />
-            <button
-              className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 text-white rotate-90"
-              onClick={closeSidebar}
-            >
-              Cerrar Filtros
-            </button>
-          </div>
-        )}
-        <div className="w-3/4 sm:w-full relative">
-          <div className="mb-4 sm:w-[full] p-5">
-            <div className="mt-5 m-auto pt-11 md:w-[80%] md:h-64">
-              <Carousel slides={slides} />
-            </div>
-            <Searchbar />
-          </div>
+          <span className="text-white text-sm font-semibold px-2 transform -rotate-90">
+            FILTROS
+          </span>
         </div>
       </div>
-    </>
+      {isSidebarExpanded && (
+        <div className="w-1/6 bg-porange md:block sm:absolute top-0 left-0 h-full z-10 transition-all duration-300 relative">
+          <Sidebar />
+          <button
+            className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 text-white rotate-90"
+            onClick={closeSidebar}
+          >
+            Cerrar Filtros
+          </button>
+        </div>
+      )}
+      <div className="w-3/4 sm:w-full relative flex flex-col items-center overflow-x-auto">
+        <div className="mb-4 sm:w-[full] p-5">
+          <div className="mt-5 m-auto pt-11 md:w-[80%] md:h-64">
+            <Carousel slides={slides} />
+          </div>
+          <Searchbar />
+        </div>
+        <Gallery />
+      </div>
+    </div>
   );
 }
 
