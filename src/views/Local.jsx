e68ea86909f77e8c8b1e90b69599e75487aa0716
Renato Francisco Báez Lobos <panchobaez@gmail.com>
2024-03-25 00:34:57 -0300

    feat: chat components

--- src/views/Local.jsx
+++ src/views/Local.jsx
@@ -1,9 +1,14 @@
+/*
+import { useParams } from "react-router-dom";
+*/
+import Chat from "../components/Chat";
 function Local() {
+ // const { id } = useParams()
   return (
-    <>
-      <p>Local</p>
-    </>
+    <div>
+      <h1>Local</h1>
+      <Chat/>
+    </div>
   );
 }
-
 export default Local;
