aceaff699f4729052787b53dce79bbcf7e3eb789
Javier Figueroa <javiernfigueroa@gmail.com>
2024-03-25 09:11:42 -0300

    socket client dependecy

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,12 @@
+
 function card() {
-  return (
-    <>
-      <div className="flex-col justify-between m-5 border-2 border-porange w-72 h-96 rounded gap-5">
-        <p className="flex-auto text-center text-2xl m-3">Titulo Bar</p>
-        <img
-          className="w-64 h-72 flex m-3 pl-1 rounded"
-          src="https://i.pinimg.com/originals/b0/cb/52/b0cb52a3da954d39ddd4f8e49ec8cb30.jpg"
-          alt="imagen"
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex-col justify-between m-5 border-2 border-porange w-72 h-96 rounded gap-5">
+                <p className="flex-auto text-center text-2xl m-3">Titulo Bar</p>
+                <img className="w-64 h-72 flex m-3 pl-1 rounded" src="https://i.pinimg.com/originals/b0/cb/52/b0cb52a3da954d39ddd4f8e49ec8cb30.jpg" alt="imagen" />
+            </div>
+        </>
+    );
 }
-export default card;
+export default card
