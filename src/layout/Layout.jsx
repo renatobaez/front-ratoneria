9b6d6710b3012ba57c3ffcb295335606cd27e0ba
Javier Figueroa <javiernfigueroa@gmail.com>
2024-03-26 16:59:04 -0300

    fix not found background

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen bg-[#f1f1f1]">
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,9 +1,16 @@
 function NotFound() {
   return (
-    <section className="w-full h-screen flex items-center justify-center">
+    <section
+      className="w-full h-screen flex items-center justify-center"
+      style={{
+        backgroundImage: "url('/bg_black.JPG')",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <img
-        className="lg:w-[37%] w-[80%]"
-        src="./bg_not_found_transparent.PNG"
+        className="lg:w-[35%] w-[80%] border-2 rounded-2xl"
+        src="./bg_not_found_2.JPG"
         alt="not found pic"
       />
     </section>
