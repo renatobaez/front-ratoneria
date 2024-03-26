351ba937a17d47072a824a2a06e1b9e9e6047ce5
samkee1001~ <samkeetangcl@gmail.com>
2024-03-25 21:13:48 -0300

    Fix Register and Login

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,23 +1,24 @@
-import Button from "../components/ui/Button"
+import Button from "../components/ui/Button";
 
 function Avatar() {
-    return (
-        <>
-        <div className="flex items-center justify-center mt-5 mb-5">
-            <div className="flex gap-5">
-                <img src="" className="flex-none w-12 h-12 rounded-full" />
-            <div>
-                <span className="block text-xl text-pgray font-semibold">Nombre de usuario</span>
-                <span className="block text-xl text-pgray">@Nickname</span>
-            </div>
-            <div>
+  return (
+    <>
+      <div className="flex items-center justify-center mt-5 mb-5">
+        <div className="flex gap-5">
+          <img src="" className="flex-none w-12 h-12 rounded-full" />
+          <div>
+            <span className="block text-xl text-pgray font-semibold">
+              Nombre de usuario
+            </span>
+            <span className="block text-xl text-pgray">@Nickname</span>
+          </div>
+          <div>
             <Button>publicar</Button>
-            </div>
-            </div>
-                
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Avatar;
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -73,7 +73,7 @@ function Login() {
             <input
               type="text"
               placeholder="Ingresa tu email"
-              className="w-full pl-12 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              className="w-full pr-12 pl-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             />
             <svg
               className="w-6 h-6 text-gray-400 absolute right-3 inset-y-0 my-auto"
