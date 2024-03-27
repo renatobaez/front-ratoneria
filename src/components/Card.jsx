dc450845bb6e8a267fe481bd0c2672c50ba42889
Javier Figueroa <javiernfigueroa@gmail.com>
2024-03-27 19:00:12 -0300

    fix colores texto cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,25 @@
-import Tabs from "../components/Tabs"
+import Tabs from "../components/Tabs";
 import Star from "./Star";
 function Card() {
-    return (
-        <div className="border-2 border-porange flex flex-col">
-            <div className=" flex flex-col m-5 w-[80%] h-{80%] rounded gap-5 items-center self-center">
-                <p className="flex-auto text-center text-2xl m-3 ">Titulo Bar</p>
-                <img className="w-[80%] h-[80%] flex m-3 pl-1 rounded" src="https://i.pinimg.com/originals/b0/cb/52/b0cb52a3da954d39ddd4f8e49ec8cb30.jpg" alt="imagen" />
-            </div>
-            <div>
-                <Star></Star>
-            </div>
-            <div>
-                <Tabs></Tabs>
-            </div>
-        </div>
-    );
+  return (
+    <div className="border-2 border-porange flex flex-col">
+      <div className=" flex flex-col m-5 w-[80%] h-{80%] rounded gap-5 items-center self-center">
+        <p className="flex-auto text-center text-2xl m-3 text-white">
+          Titulo Bar
+        </p>
+        <img
+          className="w-[80%] h-[80%] flex m-3 pl-1 rounded"
+          src="https://i.pinimg.com/originals/b0/cb/52/b0cb52a3da954d39ddd4f8e49ec8cb30.jpg"
+          alt="imagen"
+        />
+      </div>
+      <div>
+        <Star></Star>
+      </div>
+      <div>
+        <Tabs></Tabs>
+      </div>
+    </div>
+  );
 }
-export default Card
+export default Card;
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,45 +1,43 @@
 import { useState } from "react";
 
 const tabsData = [
-    {
-        label: "Precios",
-        content:    
-        "Chorrillana: $9990"
-        ,
-    },
-    {
-        label: "Comentarios",
-        content:
-        "Tabien",
-    },
+  {
+    label: "Precios",
+    content: "Chorrillana: $9990",
+  },
+  {
+    label: "Comentarios",
+    content: "Tabien",
+  },
 ];
 
 function Tabs() {
-    const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
 
-    return (
-        <div className="ml-5">
-        <div className="flex gap-3 ml-4">
+  return (
+    <div className="ml-5 text-white">
+      <div className="flex gap-3 ml-4 ">
         {tabsData.map((tab, idx) => {
-            return (
+          return (
             <button
-                key={idx}
-                className={`py-2 border-b-4 transition-colors duration-300 ${
+              key={idx}
+              className={`py-2 border-b-4 transition-colors duration-300 ${
                 idx === activeTabIndex
-                    ? "border-porange"
-                    : "border-transparent hover:border-porange"
-                }`}
-                onClick={() => setActiveTabIndex(idx)}>
-                {tab.label}
+                  ? "border-porange"
+                  : "border-transparent hover:border-porange"
+              }`}
+              onClick={() => setActiveTabIndex(idx)}
+            >
+              {tab.label}
             </button>
-            );
+          );
         })}
-        </div>
-        <div className="py-4 ml-4">
+      </div>
+      <div className="py-4 ml-4">
         <p>{tabsData[activeTabIndex].content}</p>
-        </div>
+      </div>
     </div>
-    );
+  );
 }
 
-export default Tabs
+export default Tabs;
