43204e1a19a37a53d227ba4dc962623a92199f0d
TomasMunozC <tomasmunozc@gmail.com>
2024-03-27 18:51:16 -0300

    feat local

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import Tabs from "../components/Tabs"
 import Star from "./Star";
-function card() {
+function Card() {
     return (
-        <div className="border-2 border-porange">
-            <div className=" flex flex-col justify-between m-5 w-72 h-96 rounded gap-5">
-                <p className="flex-auto text-center text-2xl m-3">Titulo Bar</p>
-                <img className="w-64 h-72 flex m-3 pl-1 rounded" src="https://i.pinimg.com/originals/b0/cb/52/b0cb52a3da954d39ddd4f8e49ec8cb30.jpg" alt="imagen" />
+        <div className="border-2 border-porange flex flex-col">
+            <div className=" flex flex-col m-5 w-[80%] h-{80%] rounded gap-5 items-center self-center">
+                <p className="flex-auto text-center text-2xl m-3 ">Titulo Bar</p>
+                <img className="w-[80%] h-[80%] flex m-3 pl-1 rounded" src="https://i.pinimg.com/originals/b0/cb/52/b0cb52a3da954d39ddd4f8e49ec8cb30.jpg" alt="imagen" />
             </div>
             <div>
                 <Star></Star>
@@ -13,8 +13,7 @@ function card() {
             <div>
                 <Tabs></Tabs>
             </div>
-            
         </div>
     );
 }
-export default card
+export default Card
