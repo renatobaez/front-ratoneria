8916ee403f988b83f29f86a5786061190cc3a0b8
Javier Figueroa <javiernfigueroa@gmail.com>
2024-03-27 18:54:08 -0300

    fix gallery grid

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -19,7 +19,7 @@ const Gallery = () => {
   ];
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
       {cards}
     </div>
   );
