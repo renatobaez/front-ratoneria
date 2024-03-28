efa43ec9bb18bd43e8be0351f9469200024bd99b
Javier Figueroa <javiernfigueroa@gmail.com>
2024-03-28 18:52:16 -0300

    fix warnings

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -7,14 +7,14 @@ const Gallery = () => {
     <Card key={2} />,
     <Card key={3} />,
     <Card key={4} />,
-    <Card key={1} />,
-    <Card key={2} />,
-    <Card key={3} />,
-    <Card key={4} />,
-    <Card key={1} />,
-    <Card key={2} />,
-    <Card key={3} />,
-    <Card key={4} />,
+    <Card key={5} />,
+    <Card key={6} />,
+    <Card key={7} />,
+    <Card key={8} />,
+    <Card key={9} />,
+    <Card key={10} />,
+    <Card key={11} />,
+    <Card key={12} />,
     // Puedes agregar más tarjetas según sea necesario
   ];
 
