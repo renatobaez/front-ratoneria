ca6fc378a7f160f4f4a8df7c60904b7f32acccb0
Javier Figueroa <javiernfigueroa@gmail.com>
2024-04-02 12:33:45 -0300

    se retiran fragmentos  innecesarios

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,13 +3,11 @@ import Footer from '../components/Footer';
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <div className="flex flex-col min-h-screen bg-[#000]">
-        <Navbar />
-        <main className="flex-1">{children}</main>
-        <Footer />
-      </div>
-    </>
+    <div className="flex flex-col min-h-screen bg-[#000]">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
   );
 };
 
